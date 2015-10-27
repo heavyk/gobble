21584f8adcece273a03d226342a90f7eed302814
flames of love <mechanicofthesequence@gmail.com>
2015-10-27 19:24:36 +0100

    commit dist

--- example/gobblefile.js
+++ example/gobblefile.js
@@ -4,6 +4,8 @@ module.exports = gobble([
 
 	// the main index.html file, and the turkey logo
 	gobble( 'src/root' ),
+	
+	gobble( 'src/lala.txt' ),
 
 	// styles - convert from SCSS to CSS using the gobble-sass plugin
 	gobble( 'src/styles' ).transform( 'sass', { src: 'main.scss', dest: 'main.css' }),
@@ -12,3 +14,12 @@ module.exports = gobble([
 	gobble( 'src/coffee' ).transform( 'coffee' ).transform( 'uglifyjs' ).transform( 'sorcery' )
 
 ]);
+
+process.on('uncaughtException', function (err) {
+	console.log('unhandled', err.stack)
+})
+
+process.on("unhandledRejection", function(reason, p){
+  console.log("Unhandled", reason, p); // log all your errors, "unsuppressing" them.
+  throw reason; // optional, in case you want to treat these as errors
+});
